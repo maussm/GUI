9a7ea67de0873184eecce500273852a74cd31620
avokjart <slobodance15@gmail.com>
2022-05-09 13:47:19 +0200

    Fixat kalender i statistik-sidan.

---
+++ script.js
@@ -0,0 +1,8 @@
+$( function() {
+    $( "#datepicker" ).datepicker({
+      showOn: "button",
+      buttonImage: "static/kalender.jpeg",
+      buttonImageOnly: true,
+      buttonText: "Select date"
+    });
+  } );
