7e630424c52ece11ff79c7ff8988912d38fd9859
Emile <emile@ecspresso.com>
2022-05-19 15:26:06 +0200

    Start på att filtrera användare.

--- static/ajax.js
+++ static/ajax.js
@@ -112,3 +112,82 @@ function getActivity(){
     });
 
 }
+
+// on success..
+let response = [{
+        'unocode': 1,
+        'name': 'emile'
+    },
+    {
+        'unocode': 2,
+        'name': 'slobo'
+    },
+    {
+        'unocode': 3,
+        'name': 'nicole'
+    },
+    {
+        'unocode': 4,
+        'name': 'vincent'
+    },
+    {
+        'unocode': 5,
+        'name': 'jens'
+    },
+    {
+        'unocode': 6,
+        'name': 'anders'
+    }
+]
+
+response.sort(function(a, b) {
+    console.log(a.name);
+    if (a.name > b.name) {
+        return 1;
+    } else if (a.name < b.name) {
+        return -1;
+    }
+    return 0;
+});
+
+response.forEach(function(val) {
+    $main = $('<p>');
+    $sub = $('<p>');
+    $main.attr('data-unocode', val.unocode);
+    $main.attr('data-name', val.name);
+    $main.text(val.name);
+
+    $sub.attr('data-unocode', val.unocode);
+    $sub.text(val.name);
+    $sub.addClass('not-selected');
+
+
+    $main.click(function() {
+        $(this).toggleClass('clicked');
+        $(`#chosencontainer > p[data-unocode=${val.unocode}]`).toggleClass('not-selected');
+    });
+
+    $sub.click(function() {
+        $(this).toggleClass('not-selected');
+        $(`#buttoncontainer > p[data-unocode=${val.unocode}]`).toggleClass('clicked');
+    });
+
+    $('#buttoncontainer').append($main);
+    $('#chosencontainer').append($sub);
+});
+
+
+let tags = $('p[data-name');
+$('#search').bind('input', function() {
+    let val = $.trim(this.value.toLowerCase()); // Det här det man skriver in när man söker.
+    tags.addClass('hidden');
+
+    tags.filter(function() {
+        return $(this).data('name').match(val) !== null;
+    }).removeClass('hidden');
+});
+
+$('#deletebutton').click(function() {
+    $('#search').val("");
+    tags.removeClass('hidden');
+});
