45194f85bf7d43520e7ea82062e2bda00370b147
avokjart <slobodance15@gmail.com>
2022-05-19 14:18:27 +0200

    Ajax GET funktion till följa upp sidan

--- static/ajax.js
+++ static/ajax.js
@@ -77,3 +77,38 @@ function postActivity() {
         }
     });
 }
+
+function getActivity(){
+    Date.prototype.yyyymmdd = function() {
+        let mm = this.getMonth() + 1; // getMonth() is zero-based
+        let dd = this.getDate();
+  
+        return [this.getFullYear(),
+                (mm>9 ? '' : '0') + mm,
+                (dd>9 ? '' : '0') + dd
+               ].join('-');
+      };
+
+    let data = {
+        "reportedDate":    date.yyyymmdd(),
+        "occurrenceDate":  $("#occurrence_date").val(),
+        "costCenterId":      $("[ccid]").attr("ccid"),
+        "tkActivityId":      $("#aktivitet").val(),
+        "participants":     $("#antal").val()   
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "/api/activity",
+        contentType: "application/json; charset=utf-8",
+        datatype: "json",
+        data: JSON.stringify(data),
+        success: function(response) {
+            console.log(response);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+}
